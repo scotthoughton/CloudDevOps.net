3bc1aa78167632bd620107a622a0ff9023c01416
Scott W Houghton <scott.houghton@live.com>
2016-09-29 13:10:53 -0400

    Added Comments to Scripts

--- Powershell/Get-WindowsTimeService/Get-WindowsTimeService.ps1
+++ Powershell/Get-WindowsTimeService/Get-WindowsTimeService.ps1
@@ -1,3 +1,49 @@
+<#   
+.SYNOPSIS   
+Script that returns current TimeZone on a computer
+    
+.DESCRIPTION 
+This script uses the Schedule.Service Windows Registry to query the local or a remote computer in order to gather the current TimeZones
+ 
+.PARAMETER Servers
+The computer that will be queried by this script, local administrative permissions are required to query this information
+
+.NOTES   
+Name: Get-WindowsTimeService.ps1
+Author: Scott W Houghton
+DateCreated: 2016-09-23
+DateUpdated: 2016-09-29
+Site: http://CloudDevOps.net
+Version: 1.2.0
+
+.LINK
+http://CloudDevOps.net
+
+.EXAMPLE
+	.\Get-WindowsTimeService.ps1 $Servers server01
+
+Description
+-----------
+This command will query server01 and displays the current TimeZone on that computer
+
+.EXAMPLE
+	.\Get-WindowsTimeService.ps1 $Servers server01,server02,server03
+
+Description
+-----------
+This command will query server01,server02,server03 and displays the current TimeZone on the list of computers
+
+.EXAMPLE
+	.\Get-WindowsTimeService.ps1 $Servers $env:COMPUTERNAME
+
+Description
+-----------
+This command will query localhost and displays the current TimeZone on that computer
+
+
+
+
+#>
 param(
     [String[]]$Servers
 )
